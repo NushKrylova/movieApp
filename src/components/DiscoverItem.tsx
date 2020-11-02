f8e3c5ecc18e719161f5c0be6191126408c971e3
Anna Krylova <annakry@wix.com>
2020-11-02 23:34:04 +0200

    fix

--- src/components/DiscoverItem.tsx
+++ src/components/DiscoverItem.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { formatDate, Movie } from '../api/tmdb'
 import { Link } from 'react-router-dom';
 import { Card } from 'react-bootstrap';
+import styles from './DiscoverItem.module.css';
 
 type DiscoverItemProps = {
     movie: Movie;
@@ -10,13 +11,13 @@ type DiscoverItemProps = {
 function DiscoverItem(props: DiscoverItemProps) {
     return (
         <Card className="h-100">
-            <Link to={"/" + props.movie.id}>
-                <Card.Img variant="top" src={props.movie.poster_path} />
+            <Link style={{height: '358px'}} to={"/" + props.movie.id}>
+                <Card.Img className="h-100 border-bottom" variant="top" src={props.movie.poster_path} />
             </Link>
             <Card.Body>
-                <Card.Title>{props.movie.vote_average}</Card.Title>
-                <Card.Title className='h3'>{props.movie.title}</Card.Title>
-                <Card.Text>{formatDate(props.movie.release_date, 'short')}</Card.Text>
+                <p className={styles.ButtonVotes}>{props.movie.vote_average}</p>
+                <p>{props.movie.title}</p>
+                <p className='text-muted'>{formatDate(props.movie.release_date, 'short')}</p>
             </Card.Body>
         </Card>
     )
