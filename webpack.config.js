eed9da98fb7e8c62d84c946d267b55d72117fbd6
Anna Krylova <annakry@wix.com>
2020-11-29 21:27:03 +0200

    webpack production configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,71 @@
 const express = require('express');
-
 const path = require('path');
+
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require("terser-webpack-plugin");
 
-module.exports = {
-    entry: {
-        app: ['./src/index.tsx'],
-        vendor: ['react', 'react-dom']
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'js/[name].bundle.js',
-        publicPath: "/movieApp/",
-    },
-    devtool: "source-map",
-    resolve: {
-        extensions: [".ts", ".tsx", ".js", ".jsx", ".json"]
-    },
-    devServer: {
-        openPage: 'movieApp/',
-        open: true,
-        contentBase: path.join(__dirname, 'dist'),
-        setup(app) {
-            app.use('/movieApp/assets', express.static('./dist/assets'))
-        }
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                loader: "ts-loader"
-            },
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    // Creates `style` nodes from JS strings
-                    'style-loader',
-                    // Translates CSS into CommonJS
-                    'css-loader',
-                    // Compiles Sass to CSS
-                    'sass-loader',
-                ],
-            },
+module.exports = function (env, argv) {
+    return {
+        mode: env.production ? 'production' : 'development',
+        devtool: env.production ? 'source-map' : 'inline-source-map',
+        entry: {
+            app: ['./src/index.tsx'],
+            vendor: ['react', 'react-dom']
+        },
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'js/[name].bundle.js',
+            publicPath: "/movieApp/",
+        },
+        resolve: {
+            extensions: [".ts", ".tsx", ".js", ".jsx", ".json"]
+        },
+        devServer: {
+            openPage: 'movieApp/',
+            open: true,
+            contentBase: path.join(__dirname, 'dist'),
+            setup(app) {
+                app.use('/movieApp/assets', express.static('./dist/assets'))
+            }
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    loader: "ts-loader"
+                },
+                {
+                    test: /\.s[ac]ss$/i,
+                    use: [
+                        {
+                            loader: MiniCssExtractPlugin.loader,
+                        },
+                        // Translates CSS into CommonJS
+                        'css-loader',
+                        // Compiles Sass to CSS
+                        'sass-loader'
+                    ],
+                },
+            ]
+        },
+        optimization: {
+            minimize: true,
+            minimizer: [
+                new TerserPlugin(),
+                new CssMinimizerPlugin(),
+            ],
+        },
+        plugins: [
+            new HtmlWebPackPlugin({
+                template: "./src/index.html"
+            }),
+            new CleanWebpackPlugin({
+                cleanOnceBeforeBuildPatterns:['**/*', '!assets/**']
+            }),
+            new MiniCssExtractPlugin(),
         ]
-    },
-
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: "./src/index.html"
-        })
-    ]
-};
+    };
+};  
